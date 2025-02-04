81df7702a34de789953026e10326f35b69c78b47
SK Ali Arman <arman@appscode.com>
2025-02-05 01:52:56 +0600

    jwt with access and refresh tokenBootstrap
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- go.mod
+++ go.mod
@@ -3,16 +3,30 @@ module github.com/sheikh-arman/go-project
 go 1.22.1
 
 require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/jwtauth v1.2.0
 	github.com/gorilla/websocket v1.5.3
+	github.com/lestrrat-go/jwx v1.2.30
 	github.com/rs/zerolog v1.33.0
+	github.com/sheikh-arman/api-server v0.0.0-20230612101249-0f4ed2766d54
 	github.com/spf13/cobra v1.8.1
 	golang.org/x/net v0.34.0
 )
 
 require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 )
---
+++ pkg/handler/jwt.go
@@ -0,0 +1,281 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwt"
+	"github.com/sheikh-arman/api-server/newsfeed"
+)
+
+var jwtkey = []byte("adsads")
+var TokenAuth *jwtauth.JWTAuth
+var tokenString string
+var token jwt.Token
+
+var ID int
+var feeds []newsfeed.Item
+var feeds2 map[int]newsfeed.Item
+var Credslist map[string]string
+var refreshTokens = make(map[string]string) // Refresh Token স্টোর করার জন্য
+func InitCred() {
+	TokenAuth = jwtauth.New(string(jwa.HS256), jwtkey, nil)
+	Credslist = make(map[string]string)
+
+	creds := []newsfeed.Credentials{
+		{
+			Username: "arman",
+			Password: "123",
+		},
+	}
+
+	for _, cred := range creds {
+		Credslist[cred.Username] = cred.Password
+	}
+}
+
+func InitDB() {
+	ID = 1
+	var feed newsfeed.Item
+	feed = newsfeed.Item{
+		Id:    ID,
+		Title: "Nothing",
+		Post:  "Lorem Ipsum Doller Site",
+	}
+	//feeds2[ID] = feed
+	ID++
+	feeds = append(feeds, feed)
+
+	feed = newsfeed.Item{
+		Id:    ID,
+		Title: "Nothing2",
+		Post:  "Lorem Ipsum Doller Site2",
+	}
+	//feeds2[ID] = feed
+	ID++
+	feeds = append(feeds, feed)
+
+	feed = newsfeed.Item{
+		Id:    ID,
+		Title: "Nothing3",
+		Post:  "Lorem Ipsum Doller Site3",
+	}
+	//feeds2[ID] = feed
+	ID++
+	feeds = append(feeds, feed)
+}
+
+func WriteJsonResponse(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func GetNewsFeeds(w http.ResponseWriter, r *http.Request) {
+	log.Println("test")
+	sort.SliceStable(feeds, func(i, j int) bool {
+		return feeds[i].Id < feeds[j].Id
+	})
+	WriteJsonResponse(w, http.StatusOK, feeds2)
+}
+
+func GetNewsFeed(w http.ResponseWriter, r *http.Request) {
+	param := chi.URLParam(r, "id")
+	paramsID, _ := strconv.Atoi(param)
+
+	for _, curFeed := range feeds {
+		if curFeed.Id == paramsID {
+			WriteJsonResponse(w, http.StatusOK, curFeed)
+			return
+		}
+	}
+	WriteJsonResponse(w, http.StatusNotFound, "Newsfeed doesn't exist")
+}
+
+func CreateNewsFeed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newFeed newsfeed.Item
+	err := json.NewDecoder(r.Body).Decode(&newFeed)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	newFeed.Id = ID
+	feeds = append(feeds, newFeed)
+	ID++
+}
+func DeleteNewsFeed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := chi.URLParam(r, "id")
+	paramsID, _ := strconv.Atoi(param)
+	for index, curFeed := range feeds {
+		if curFeed.Id == paramsID {
+			feeds = append(feeds[:index], feeds[index+1:]...)
+			break
+		}
+	}
+	//fmt.Println(feeds)
+	WriteJsonResponse(w, http.StatusOK, feeds)
+}
+func UpdateNewsFeed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := chi.URLParam(r, "id")
+	paramID, _ := strconv.Atoi(param)
+	var newFeed newsfeed.Item
+	err := json.NewDecoder(r.Body).Decode(&newFeed)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	for index, curFeed := range feeds {
+		if curFeed.Id == paramID {
+			newFeed.Id = paramID
+			feeds[index] = newFeed
+			json.NewEncoder(w).Encode(feeds)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No data")
+}
+func Login(w http.ResponseWriter, r *http.Request) {
+	var creds newsfeed.Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	correctPassword, ok := Credslist[creds.Username]
+	if !ok || creds.Password != correctPassword {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Access Token (10 মিনিট)
+	accessExpireTime := time.Now().Add(10 * time.Minute)
+	_, accessToken, err := TokenAuth.Encode(map[string]interface{}{
+		"aud": creds.Username,
+		"exp": accessExpireTime.Unix(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Refresh Token (7 দিন)
+	refreshExpireTime := time.Now().Add(7 * 24 * time.Hour)
+	_, refreshToken, err := TokenAuth.Encode(map[string]interface{}{
+		"aud": creds.Username,
+		"exp": refreshExpireTime.Unix(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Refresh Token স্টোর করা হচ্ছে
+	refreshTokens[refreshToken] = creds.Username
+
+	// Access Token রেসপন্স হিসাবে পাঠানো
+	w.Header().Set("Authorization", "Bearer "+accessToken)
+
+	// Refresh Token HTTP-Only Cookie তে পাঠানো
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    refreshToken,
+		Expires:  refreshExpireTime,
+		HttpOnly: true, // নিরাপত্তার জন্য
+	})
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	// Refresh Token Cookie মুছে ফেলা
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    "",
+		Expires:  time.Now(),
+		HttpOnly: true,
+	})
+
+	// Refresh Token মেমোরি থেকে মুছে ফেলা
+	for token := range refreshTokens {
+		delete(refreshTokens, token)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func StartServer(port int) {
+	InitCred()
+	InitDB()
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Post("/login", Login)
+	r.Post("/refresh-token", RefreshToken) // Refresh Token API
+	r.Group(func(r chi.Router) {
+		// jwtauth-> will learn later
+		r.Use(jwtauth.Verifier(TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Route("/newsfeeds", func(r chi.Router) {
+			r.Get("/", GetNewsFeeds)
+			r.Get("/{id}", GetNewsFeed)
+			r.Post("/", CreateNewsFeed)
+			r.Delete("/{id}", DeleteNewsFeed)
+			r.Put("/{id}", UpdateNewsFeed)
+		})
+		r.Post("/logout", Logout)
+	})
+	//port := 5050
+	Server := &http.Server{
+		Addr:    ":" + strconv.Itoa(port),
+		Handler: r,
+	}
+	fmt.Println("Serving on " + strconv.Itoa(port))
+	//http.ListenAndServe(strconv.Itoa(port), r)
+	fmt.Println(Server.ListenAndServe())
+}
+
+func RefreshToken(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("refresh_token")
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	refreshToken := cookie.Value
+	username, exists := refreshTokens[refreshToken]
+	if !exists {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// নতুন Access Token তৈরি
+	accessExpireTime := time.Now().Add(10 * time.Minute)
+	_, newAccessToken, err := TokenAuth.Encode(map[string]interface{}{
+		"aud": username,
+		"exp": accessExpireTime.Unix(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// নতুন Access Token পাঠানো
+	w.Header().Set("Authorization", "Bearer "+newAccessToken)
+	w.WriteHeader(http.StatusOK)
+}
