5f75d17ef50be2e6da0f45e522409692133b063a
SK Ali Arman <arman@appscode.com>
2025-03-20 02:19:46 +0600

    load test
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 	"os"
+	"sync"
 	"time"
 )
+
+// User model
+type User struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"size:255"`
+	Age  int
+}
 
 func DbConnect() {
 	user := os.Getenv("MYSQL_ROOT_USER")
@@ -50,6 +59,7 @@ func DbConnect() {
 	//}
 	showDatabases(db)
 	fmt.Println("connected to database, hurrah!!!")
+	DatabaseLoadTest(db)
 }
 
 func showDatabases(db *gorm.DB) {
@@ -58,5 +68,91 @@ func showDatabases(db *gorm.DB) {
 	fmt.Println("databaseList:-")
 	for i, database := range databaseList {
 		fmt.Println(i, database)
+	}
+}
+
+func DatabaseLoadTest(db *gorm.DB) {
+
+	ReadTest(db)
+	WriteTest(db)
+}
+
+func ReadTest(db *gorm.DB) {
+	// Number of concurrent requests
+	numWorkers := 100000
+	wg := sync.WaitGroup{}
+
+	start := time.Now()
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			testQuery(db, id)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Total time taken for read:", time.Since(start))
+}
+
+func testQuery(db *gorm.DB, id int) {
+	var count int64
+	result := db.Raw("SELECT COUNT(*) FROM user").Scan(&count)
+	if result.Error != nil {
+		log.Println("Query failed:", result.Error)
+	} else {
+		log.Printf("Worker %d: User count = %d\n", id, count)
+	}
+}
+
+func WriteTest(db *gorm.DB) {
+	user := os.Getenv("MYSQL_ROOT_USER")
+	password := os.Getenv("MYSQL_ROOT_PASSWORD")
+	host := os.Getenv("MYSQL_ROOT_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("MYSQL_ROOT_PORT")
+	if port == "" {
+		port = "3306"
+	}
+	fmt.Println("MYSQL_ROOT_USER:", user, "MYSQL_PASSWORD", password, "MYSQL_HOST", host, "MYSQL_PORT", port)
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, "testDB")
+	fmt.Println("connecting to database...", connectionString)
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Migrate schema
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Number of concurrent writes
+	numWorkers := 100
+	numRecords := 1000 // Total records to insert
+	wg := sync.WaitGroup{}
+
+	start := time.Now()
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			insertRecords(db, workerID, numRecords)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Total time taken for write:", time.Since(start))
+}
+
+func insertRecords(db *gorm.DB, workerID, num int) {
+	for i := 0; i < num; i++ {
+		user := User{Name: fmt.Sprintf("User_%d_%d", workerID, i), Age: 20 + (i % 10)}
+		if err := db.Create(&user).Error; err != nil {
+			log.Printf("Worker %d: Insert failed: %v", workerID, err)
+		}
 	}
 }
